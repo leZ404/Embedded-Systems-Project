ca28c16753035ac4aa383e4b427e299bd1cf0251
Ryan Lahbabi <ryan.lahbabi@polymtl.ca>
2022-03-31 12:13:21 -0400

    Ajout classe photoresistance

---
+++ tp/proojetFinal/lib/photoresistance .h
@@ -0,0 +1,16 @@
+#define F_CPU 8000000U
+#include <avr/io.h>
+#include <util/delay.h>
+#include <can.h>
+#include <avr/io.h> 
+#include <avr/interrupt.h>
+
+class Photoresistance {
+public:
+getDroit();
+private:
+
+
+
+
+} 
---
+++ tp/proojetFinal/lib/photoresistance.cpp
@@ -0,0 +1,6 @@
+#define F_CPU 8000000U
+#include <avr/io.h>
+#include <util/delay.h>
+#include <photoresistance.h>
+
+
